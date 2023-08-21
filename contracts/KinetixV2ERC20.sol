e889393de4e12d7ed37bbccc81707aa1c67622d1
patrickroyme <patrickroyme@gmail.com>
2023-08-21 13:03:42 +0300

    Initial changes. @kinetix/* namespace applied.

--- contracts/KinetixV2ERC20.sol
+++ contracts/KinetixV2ERC20.sol
@@ -3,13 +3,13 @@ pragma solidity =0.5.16;
 import './interfaces/IUniswapV2ERC20.sol';
 import './libraries/SafeMath.sol';
 
-contract UniswapV2ERC20 is IUniswapV2ERC20 {
+contract KinetixV2ERC20 is IUniswapV2ERC20 {
     using SafeMath for uint;
 
-    string public constant name = 'Uniswap V2';
-    string public constant symbol = 'UNI-V2';
+    string public constant name = 'Kinetix V2';
+    string public constant symbol = 'KFI-V2';
     uint8 public constant decimals = 18;
-    uint  public totalSupply;
+    uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
 
@@ -79,7 +79,7 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
     }
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
-        require(deadline >= block.timestamp, 'UniswapV2: EXPIRED');
+        require(deadline >= block.timestamp, 'KinetixV2: EXPIRED');
         bytes32 digest = keccak256(
             abi.encodePacked(
                 '\x19\x01',
@@ -88,7 +88,7 @@ contract UniswapV2ERC20 is IUniswapV2ERC20 {
             )
         );
         address recoveredAddress = ecrecover(digest, v, r, s);
-        require(recoveredAddress != address(0) && recoveredAddress == owner, 'UniswapV2: INVALID_SIGNATURE');
+        require(recoveredAddress != address(0) && recoveredAddress == owner, 'KinetixV2: INVALID_SIGNATURE');
         _approve(owner, spender, value);
     }
 }
--- contracts/KinetixV2Factory.sol
+++ contracts/KinetixV2Factory.sol
@@ -1,9 +1,9 @@
 pragma solidity =0.5.16;
 
 import './interfaces/IUniswapV2Factory.sol';
-import './UniswapV2Pair.sol';
+import './KinetixV2Pair.sol';
 
-contract UniswapV2Factory is IUniswapV2Factory {
+contract KinetixV2Factory is IUniswapV2Factory {
     address public feeTo;
     address public feeToSetter;
 
@@ -25,7 +25,7 @@ contract UniswapV2Factory is IUniswapV2Factory {
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
         require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
-        bytes memory bytecode = type(UniswapV2Pair).creationCode;
+        bytes memory bytecode = type(KinetixV2Pair).creationCode;
         bytes32 salt = keccak256(abi.encodePacked(token0, token1));
         assembly {
             pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
